7eea29080e828ab5dc785c9b4a290e661f86fc08
peetaaah <peterkwc82@gmail.com>
2023-03-05 23:13:46 +0900

    updated downloads paths and new route

--- app.js
+++ app.js
@@ -60,9 +60,28 @@ app.get("/upload", (req, res) => {
 
 // --------------download section ----------- //
 
-app.get("/download", (req, res) => {
-    res.render('download')
-});
+app.get("/Images/:id.:ext", (req, res) => {
+    const file = `${req.params.id}.${req.params.ext}`;
+    const path = `./Images/${file}`;
+    const readStream = fs.createReadStream(path);
+    if (!fs.existsSync(path)) {
+      res.status(404).send("File not found");
+    } else {
+      readStream.on("open", () => {
+        // set the content type and attachment header
+        res.setHeader("Content-Type", "application/octet-stream");
+        res.setHeader("Content-Disposition", "attachment; filename=" + file);
+  
+        // pipe the read stream to the response object
+        readStream.pipe(res);
+      });
+  
+      readStream.on("error", (err) => {
+        res.end(err);
+      });
+      // res.sendFile(path, {root: __dirname});
+    }
+  });
 
 
 // --------------end download section ----------- //
