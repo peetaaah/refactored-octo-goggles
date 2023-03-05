cdccd3c468892d4c60c59717634dfe66a6740c48
peetaaah <peterkwc82@gmail.com>
2023-03-05 21:52:23 +0900

    initial project start

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # refactored-octo-goggles
 terrascope test. had to throw off other people, haha
+
+## Notes:
+I'm aware that there's a bunch of stuff that is somewhat messy, but I'll refactor as best as I can as the days go on.
+
+
+## Packages:
+1. Multer.
+2. Express.
+3. UUID.
+4. Nodemon (Dev dependency, just for easy testing / hot reloads)
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const { uuid } = require("uuidv4");
+const port = 3050;
+const fs = require("fs");
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: "./Images",
+  filename: (req, file, callback) => {
+    console.log(file);
+    callback(null, uuid().slice(0, 7) + path.extname(file.originalname));
+  },
+});
+
+//  -------- file filtering for images -------------- //
+let fileFilter = function (req, file, cb) {
+  console.log(file.mimetype);
+  const allowedMimes = ["image/jpeg", "image/jpg", "image/png", "image/gif", "image/svg+xml"];
+
+  if (allowedMimes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    return cb(new Error("only images are allowed!"));
+    // this is so that anything other than the above images will throw an error and crash this.
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
+  "image"
+);
+
+// --------------main section ----------- //
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+// --------------end main section ----------- //
+// --------------upload section ----------- //
+
+
+// --------------end upload section ----------- //
+
+// --------------download section ----------- //
+
+// --------------end download section ----------- //
+
+// app listener
+app.listen(port);
+console.log("listening on port" + `${port}`);
