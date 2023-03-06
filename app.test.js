7fe71069e01929976a40f6be5366ad1a4b2c7734
petaar <peterkwc82@gmai.com>
2023-03-06 22:00:24 +0900

    added the 'GET /' tests first.

--- app.test.js
+++ app.test.js
@@ -7,3 +7,15 @@ describe("GET /", () => {
         expect(response.statusCode).toBe(200)
     })
 })
+describe("GET /upload", () => {
+    test('it should respond with 200 code', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200)
+    })
+})
+describe("GET /download", () => {
+    test('it should respond with 200 code', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200)
+    })
+})
