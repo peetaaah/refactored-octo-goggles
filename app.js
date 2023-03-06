7dfdab5e6f1e5084d7111531795fe0839546c321
petaar <peterkwc82@gmai.com>
2023-03-06 20:58:34 +0900

    refactored routes

--- app.js
+++ app.js
@@ -9,65 +9,25 @@ const testRoute = require("./routes/test");
 
 const app = express();
 
-const storage = multer.diskStorage({
-  destination: "./Images",
-  filename: (req, file, callback) => {
-    console.log(file);
-    callback(null, uuid().slice(0, 7) + path.extname(file.originalname));
-    // note, i used a slice here to reduce the overall length of the uuid.
-    // its just to make it look cleaner, but we can remove it easy.
-  },
-});
-
-//  -------- file filtering for images -------------- //
-let fileFilter = function (req, file, cb) {
-  console.log(file.mimetype);
-  const allowedMimes = [
-    "image/jpeg",
-    "image/jpg",
-    "image/png",
-    "image/gif",
-    "image/svg+xml",
-  ];
-
-  if (allowedMimes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    return cb(new Error("only images are allowed!"));
-    // this is so that anything other than the above images will throw an error and crash this.
-  }
-};
-
-const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
-  "image"
-);
-
-// --------------main section ----------- //
+// main section
 
 app.set("view engine", "ejs");
-// also using ejs for easy viewing
 
 app.get("/", (req, res) => {
   res.render("main");
 });
 
-// --------------end main section ----------- //
 
-// --------------upload section ------------ //
+// upload section
 
-// app.get("/upload", (req, res) => {
-//     res.render("upload");
-//   });
-
-
-app.post("/upload", upload, (req, res) => {
-  res.render("uploaded")
-  console.log(req.file.buffer);
-});
 app.get("/upload", uploadRoute);
-// --------------end upload section ----------- //
+app.post("/upload", uploadRoute);
+
+// test section
+
 app.get("/test", testRoute);
-// --------------download section ----------- //
+
+// download section
 
 app.get("/download", (req, res) => {
   res.render("download");
@@ -92,11 +52,8 @@ app.get("/Images/:id.:ext", (req, res) => {
     readStream.on("error", (err) => {
       res.end(err);
     });
-    // res.sendFile(path, {root: __dirname});
   }
 });
-
-// --------------end download section ----------- //
 
 // app listener
 app.listen(port);
--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const path = require("path");
 const { uuid } = require("uuidv4");
 
 const router = express.Router();
@@ -15,15 +16,37 @@ const storage = multer.diskStorage({
 });
 
 
-const upload = multer({ storage });
+let fileFilter = function (req, file, cb) {
+  console.log(file.mimetype);
+  const allowedMimes = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/gif",
+    "image/svg+xml",
+  ];
+
+  if (allowedMimes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    return cb(new Error("only images are allowed!"));
+    // this is so that anything other than the above images will throw an error and crash this.
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
+  "image"
+);
+
 
 
 router.get('/upload', (req, res) => {
+  console.log()
   res.render('upload');
 });
 
 // post
-router.post('/upload', upload.single('image'), (req, res) => {
+router.post('/upload', upload, (req, res) => {
   res.render('uploaded');
 });
 
